dd0837c76c9e94be253feb53ed45180162edf5c9
OlgaLavrishcheva <76204278+OlgaLavrishcheva@users.noreply.github.com>
2021-05-09 14:52:13 +0300

    Created Viewing details List heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -16,6 +16,11 @@ export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
 
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   constructor() {
   }
 
