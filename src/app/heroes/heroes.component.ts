fe4896aecf5cd74f71560cec447f1531687ef831
OlgaLavrishcheva <76204278+OlgaLavrishcheva@users.noreply.github.com>
2021-05-10 13:07:15 +0300

    Added Observable HeroService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -27,7 +27,8 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   ngOnInit(): void {
