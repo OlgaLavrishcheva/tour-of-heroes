6d415d4db2e4c13093ba9ab4ff106cad15f0a767
OlgaLavrishcheva <76204278+OlgaLavrishcheva@users.noreply.github.com>
2021-05-10 13:33:11 +0300

    Implemented the message from HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,6 +13,7 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 
